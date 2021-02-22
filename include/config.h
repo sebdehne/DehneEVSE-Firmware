86a480ceb48fce263f63b7090cefcc150b52d743
Sebastian Dehne <sebastian@dehnes.com>
2021-02-22 23:09:19 +0100

    code for 1kHz PWM signal

--- include/config.h
+++ include/config.h
@@ -7,6 +7,9 @@ void connectToWifi();
 u_int32_t toInt(byte buf[]);
 void intToByteArray(uint32_t input, byte dst[]);
 
+void PWM_1kHz_tcc0_PIN_D3_start(uint8_t dutyCycle);
+void PWM_1kHz_tcc0_PIN_D3_adjust_duty_cycle(uint8_t dutyCycle);
+
 #define REQUEST_TYPE_PING 1
 #define REQUEST_TYPE_FIRMWARE 2
 #define RESPONSE_TYPE_PONG 1
